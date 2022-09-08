273de819a1e2d4b53b3e00ae5c24fcd604f707b2
abbie-dev1 <sitholeabbie@gmail.com>
2022-09-08 15:59:10 +0200

    UI added

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,12 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { employees } from "./components/data";
+import { getData } from "./components/employees";
+import "./App.css";
 
-function App() {
+// function App() {
+//   return (
+//     <h1 className="text-3xl font-bold">
+//       Hello world!
+//     </h1>
+//   );
+// }
+
+// export default App;
+
+export default function List() {
+  const listItems = employees.map((person) => (
+    <div key={person.id} className="card w-96 bg-base-100 shadow-xl">
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{person.name}:</h2>
+        <p>{" " + person.profession + " "} known for {person.accomplishment}</p>
+      </div>
+      <figure>
+        <img src={getData(person)} alt={person.name} />
+      </figure>
+      <div className="card-actions-center text-center">
+      <button className="btn btn-sm">Edit</button>
+      <button className="btn btn-sm">Delete</button>
+    </div>
+    </div>));
   return (
-    <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
+    <article>
+      <h1>Employees</h1>
+      <ul>{listItems}</ul>
+    </article>
   );
 }
-
-export default App;
---
+++ crud/src/components/employees.jsx
@@ -0,0 +1,7 @@
+export function getData(employees) {
+    return (
+      'https://i.imgur.com/' +
+      employees.imageId + 's.jpg'
+    );
+  }
+  
